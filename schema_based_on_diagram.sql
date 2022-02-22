6d63f5c442a8b1839b3deed26acc21e52d36b82a
TheLuisAbarca <arclaav.adm@gmail.com>
2022-02-22 14:30:21 -0500

    Add invoices table to the schema.

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,6 +1,15 @@
 CREATE TABLE patients(
   id INT GENERATED ALWAYS AS IDENTITY,
-  name VARCHAR,
-  date_of_birth DATE,
+  name VARCHAR NOT NULL,
+  date_of_birth DATE NOT NULL,
   PRIMARY KEY(id)
+);
+
+CREATE TABLE invoices (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    total_amount DECIMAL(10,2) NOT NULL,
+    generated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    payed_at TIMESTAMP NOT NULL,
+    medical_history_id INT NOT NULL,
+    PRIMARY KEY(id)
 );
